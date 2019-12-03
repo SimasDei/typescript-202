63d386e2f2ed550948b3fe8be0cd4a5cd8ddb99b
Simas <simonas.deimantavicius@mediapark.com>
2019-12-03 13:28:19 +0200

    add auto-bind typescript decorator to bind (this)

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
+/**
+ * @helpers
+ */
+function autoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunc = originalMethod.bind(this);
+      return boundFunc;
+    }
+  };
+  return adjustedDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   formElement: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
@@ -9,7 +27,25 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = "user-input";
+    this.titleInputElement = this.formElement.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector("#people") as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  @autoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.formElement.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
