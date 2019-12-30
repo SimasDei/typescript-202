515067ebb8ed03d477115b8f5106dc58f48f9524
Simas <simonas.deimantavicius@mediapark.com>
2019-12-30 11:55:40 +0200

    add Project Item class

--- src/app.ts
+++ src/app.ts
@@ -120,6 +120,32 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  constructor(hostId: string, project: Project) {
+    super('single-project', hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  configure() {}
+  renderContent() {
+    const HTMLElements = {
+      h2: this.element.querySelector('h2')!,
+      h3: this.element.querySelector('h3')!,
+      p: this.element.querySelector('p')!,
+    };
+
+    HTMLElements.h2.textContent = this.project.title;
+    HTMLElements.h3.textContent = this.project.people.toString();
+    HTMLElements.p.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -153,9 +179,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
     listEl.innerHTML = '';
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement('li');
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
     }
   }
 }
